99557dd05394378945247ab8d16147f7538ad116
Cristian Trailescu <168114629+1berruu1@users.noreply.github.com>
2025-03-08 16:09:36 +0200

    Update Comanda.java

--- MAP/Lab2/Domain/Comanda.java
+++ MAP/Lab2/Domain/Comanda.java
@@ -1,1 +1,50 @@
+package Domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class Comanda extends Tort implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private List<Tort> lista_torturi;
+    private Date data;
+
+    public Comanda(){
+        super();
+    }
+
+    public List<Tort> getLista_torturi() {
+        return lista_torturi;
+    }
+
+    public void setLista_torturi(List<Tort> lista_torturi) {
+        this.lista_torturi = lista_torturi;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder detaliiComanda = new StringBuilder("Id comanda: " + id + " Data: " + data + " Lista torturi: ");
+        for (Tort tort : lista_torturi) {
+            detaliiComanda.append(tort.getTip_tort()).append(", ");
+        }
+        return detaliiComanda.toString();
+    }
+
+
+    public Comanda(int id, String tip_tort, List<Tort> lista_torturi, Date data) {
+        super(id, tip_tort);
+        this.lista_torturi = lista_torturi;
+        this.data = data;
+    }
+}
 
