c8010acd904e2706dfa82d041e8cce49387649c3
Cristian Trailescu <168114629+1berruu1@users.noreply.github.com>
2025-03-08 16:09:16 +0200

    Update and rename Tort to Tort.java

---
+++ MAP/Lab2/Domain/Tort.java
@@ -0,0 +1,32 @@
+
+package Domain;
+
+import java.io.Serializable;
+
+public class Tort extends Entity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected String tip_tort;
+
+    public String getTip_tort() {
+        return tip_tort;
+    }
+
+    public void setTip_tort(String tip_tort) {
+        this.tip_tort = tip_tort;
+    }
+
+    public Tort() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Id Tort=" + getId() + ", Tip Tort= " + getTip_tort() + " ";
+    }
+
+    public Tort(int id, String tip_tort) {
+        super(id);
+        this.tip_tort = tip_tort;
+    }
+}
