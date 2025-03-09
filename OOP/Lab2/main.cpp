56c3e4ca69fa9d82f6b4705362cbeee064e6d653
Cristian Trailescu <168114629+1berruu1@users.noreply.github.com>
2025-03-09 20:49:34 +0200

    Update main.cpp

--- OOP/Lab2/main.cpp
+++ OOP/Lab2/main.cpp
@@ -4,11 +4,16 @@
 using namespace std;
 
 
+// This program is a console application that reads a list of integers and performs operations on it.
+// adding numbers to a sequence, displaying the sequence,
+// displaying the prime numbers in the sequence, and displaying the distinct numbers in the sequence.
+// the array for the sequence is alocated staticaly
+
 int main() {
     int n, x[100];
-//    test_distincte();
-//    test_prime();
+    test_distincte();
+    test_prime();
     console(n,x);
     return 0;
 }
- // lista 10 5 5 3 10 23 22 12 10 90 13994 233 4
+ 
