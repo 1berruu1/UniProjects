659ac8cd1a0be7e7909d327dee3091dfbc2a9cdb
Cristian Trailescu <168114629+1berruu1@users.noreply.github.com>
2025-03-09 20:56:37 +0200

    Update main.cpp

--- OOP/Lab4/main.cpp
+++ OOP/Lab4/main.cpp
@@ -3,7 +3,7 @@
 #include "tests.h"
 using namespace std;
 
-
+//This project has only the entity for a square and further versions will be able to create the squares in the console terminal
 
 int main() {
     return 0;
